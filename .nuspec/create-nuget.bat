f03295b18e3c91628867e1295dc154aba7759dbd
adun <xiaohui.fang@samsung.com>
2019-07-25 22:36:36 +0800

    Roll back to 1.0.4 and change the version to 1.0.6

--- .nuspec/create-nuget.bat
+++ .nuspec/create-nuget.bat
@@ -1,2 +1,2 @@
-nuget pack Tizen.NUI.XamlBuild.nuspec -properties configuration=debug;platform=anycpu -Version 1.0.0
+nuget pack Tizen.NUI.XamlBuild.nuspec -properties configuration=debug;platform=anycpu -Version 1.0.6
 @pause
